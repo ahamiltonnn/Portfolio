869fbf4527862605edba792a24cf0bd6158d3b3d
Austin Hamilton <ahamiltonnn@outlook.com>
2024-12-01 18:58:24 -0700

    mobile functionality

--- app/NavMenu.tsx
+++ app/NavMenu.tsx
@@ -1,29 +1,47 @@
+"use client";
+
 import Link from "next/link";
+import React, { useState } from "react";
 import styles from "./NavMenu.module.css";
 import Image from "next/image";
 
-export default function NavMenu() {
+const Nav = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen((prev) => !prev);
+  };
+
   return (
     <nav className={styles.nav}>
-      <Link href={"/"}>
-        <Image
-          src="/finalLogo.svg"
-          width={250}
-          height={50}
-          alt="AHlogo"
-        />
-      </Link>
-      <ul className={styles.links}>
-      <li>
-          <Link href={"/about"}>About</Link>
+      <div className={styles.logo}>
+        <Link href="/">
+          <Image
+            src="finalLogo.svg"
+            alt="My Portfolio Logo"
+            width={200}
+            height={50}
+          />
+        </Link>
+      </div>
+      <ul className={`${styles.links} ${isMenuOpen ? styles.active : ""}`}>
+        <li>
+          <Link href="about">About</Link>
         </li>
         <li>
-          <Link href={"/portfolio"}>Portfolio</Link>
+          <Link href="portfolio">Portfolio</Link>
         </li>
         <li>
-          <Link href={"/contact"}>Contact</Link>
+          <Link href="contact">Contact</Link>
         </li>
       </ul>
+      <div className={styles.hamburger} onClick={toggleMenu}>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
     </nav>
   );
-}
+};
+
+export default Nav;
