25390dc250b1e21b44b844d3a0a8887c777e446b
Austin Hamilton <ahamiltonnn@outlook.com>
2024-12-01 19:26:20 -0700

    fixed carcar import

--- app/CarCar/page.tsx
+++ app/CarCar/page.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import styles from "./carcar.module.css";
+import styles from "./CarCar.module.css";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
